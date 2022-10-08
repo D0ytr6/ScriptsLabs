0d6e363dda4d38ad8588857155c9207872e58357
dmytro <vasyuk.dima27@gmail.com>
2022-10-08 20:25:48 +0300

    Deleted trash

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,29 +1,3 @@
-// // підключення express
-// const express = require("express");
-// // створюємо об’єкт додатка
-// const app = express();
-// // визначаємо обробник для маршруту "/"
-// app.get("/", (request, response) => {
-    
-//     // відправляємо відповідь
-//     response.send("<h2>Привет Express!</h2>");
-// });
-
-// app.get("/about", (request, response) => {
-     
-//   response.send("<h1> About </h1>");
-// });
- 
-// app.get("/contact", (request, response) => {
-   
-//   response.send("<h1> Contacts </h1>");
-// });
-
-// // починаємо прослуховувати підключення на 3000 порту
-// app.listen(3000);
-
-
-
 require('dotenv').config();
 const express = require("express");
 const app = express();
--- Lab1/app.test.js
+++ Lab1/app.test.js
@@ -1,32 +1,3 @@
-// const app = require('./server');
-// const supertest = require('supertest');
-// const request = supertest(app);
-
-// it('gets the test endpoint', async (done) => {
-
-//     const response = await request.get('/test');
-//     done();
-//     // expect(response.status).toBe(200);
-//     // expect(response.body.message).toBe('pass!').done();
-    
-// }); 
-
-// const request = require('supertest');
-// var app = require('./server');
-// const assert = require('assert');
-
-// it('should return Hello Test', function (done) {
-//   request(app)
-//     .get('/test')
-//     .expect(200)
-//     .expect(function (response) {
-//       assert.deepEqual(response.body, {
-//         message: 'pass!',
-//       });
-//     })
-//     .end(done)
-  
-// });
 
 const request = require('supertest');
 var app = require('./app');
