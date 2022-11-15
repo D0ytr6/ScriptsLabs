996d8301806bbe48cb729b3d44b996462df66d32
dmytro <vasyuk.dima27@gmail.com>
2022-11-16 00:06:24 +0200

    Init 3 Lab

--- Lab2/proxy/start_server.js
+++ Lab2/proxy/start_server.js
@@ -2,5 +2,5 @@ const app = require('./routes')
 const conf = require('./service')
 
 app.listen(conf.get('port'), () => {
-    console.log('Server start at localhost');
+    console.log('Server start at localhost ' + conf.get('port'));
 });
